44edc7a733daed9a9ac25a10f06bca29b2a42666
frakanoGIT <frakano.7p@gmail.com>
2023-08-19 21:40:28 +0700

    Completing content

--- app/components/SkillsMe.tsx
+++ app/components/SkillsMe.tsx
@@ -1,11 +1,45 @@
 import React from "react";
+import Image from "next/image";
 
 import SideCard from "./SideCard";
 
 export default function SkillsMe() {
-    return (
-        <SideCard title="Skills">
-            <p className="h-80"></p>
-        </SideCard>
-    )
+  return (
+    <SideCard title="Skills">
+      <ul className="flex flex-wrap justify-around content-center h-80">
+        <li className="m-2">
+          <Image
+            src="/logo-html5.svg"
+            alt="HTML5"
+            width="80"
+            height="80"
+          />
+        </li>
+        <li className="m-2">
+        <Image
+            src="/logo-css3.svg"
+            alt="CSS3"
+            width="80"
+            height="80"
+          />
+        </li>
+        <li className="m-2">
+        <Image
+            src="/logo-nodejs.svg"
+            alt="Javascript"
+            width="80"
+            height="80"
+          />
+        </li>
+        <li className="m-2">
+        <Image
+            src="/logo-react.svg"
+            alt="React"
+            width="80"
+            height="80"
+          />
+        </li>
+      </ul>
+    </SideCard>
+  );
 }
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -4,7 +4,7 @@ import SideCard from "../components/SideCard";
 export default function Page() {
     return (
         <SideCard title="Projects">
-
+            <p>Nothing to show now</p>
         </SideCard>
     );
   }
